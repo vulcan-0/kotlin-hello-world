bf958d4af775d6e06ca7e5727fbf1cb5be47645b
Sam Lu <luxiaocong524@163.com>
2023-01-30 08:59:30 +0800

    feat: 添加示例代码[kotlin 委托]

---
+++ src/main/kotlin/org/vc121/light/delegated/standard/Map.kt
@@ -0,0 +1,34 @@
+package org.vc121.light.delegated.standard
+
+class Site(val map: Map<String, Any?>) {
+    val name: String by map
+    val url: String by map
+}
+
+fun main(args: Array<String>) {
+    // 构造函数接受一个映射参数
+    val site = Site(
+        mapOf(
+            "name" to "菜鸟教程",
+            "url" to "www.runoob.com"
+        )
+    )
+    // 读取映射值
+    println(site.name)
+    println(site.url)
+
+    println("--------------")
+    // 可变的 Map
+    var map: MutableMap<String, Any?> = mutableMapOf(
+        "name" to "菜鸟教程",
+        "url" to "www.runoob.com"
+    )
+    val site2 = Site(map)
+    println(site2.name)
+    println(site2.url)
+    println("--------------")
+    map.put("name", "Google")
+    map.put("url", "www.google.com")
+    println(site2.name)
+    println(site2.url)
+}
---
+++ src/main/kotlin/org/vc121/light/delegated/standard/NotNull.kt
@@ -0,0 +1,14 @@
+package org.vc121.light.delegated.standard
+
+import kotlin.properties.Delegates
+
+class Foo {
+    var notNullBar: String by Delegates.notNull<String>()
+}
+
+fun main(args: Array<String>) {
+    val foo = Foo()
+    // println(foo.notNullBar) // 属性在赋值前就被访问的话则会抛出异常
+    foo.notNullBar = "bar"
+    println(foo.notNullBar)
+}
